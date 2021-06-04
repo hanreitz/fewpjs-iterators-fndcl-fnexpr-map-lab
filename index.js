1da0764fbb40178a577c8be279b1fe7525bacc6b
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-06-04 14:28:43 -0400

    done

--- index.js
+++ index.js
@@ -12,5 +12,10 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(sentence => {
+    const sentenceAsArray = sentence.split(' ');
+    const titleCaseSentenceArray = sentenceAsArray.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+    const titleCaseSentence = titleCaseSentenceArray.join(' ');
+    return titleCaseSentence;
+  });
+};
